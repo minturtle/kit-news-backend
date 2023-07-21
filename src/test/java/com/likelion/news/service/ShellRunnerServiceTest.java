89f3d5e6d479973d4781de9d2b1312df65b8ab19
minturtle <minseok0425@kakao.com>
2023-07-21 19:37:51 +0900

    TEST : ShellRunnerService - command와 arg를 받아 실제 실행하는 명령어를 만들 수 있다.

---
+++ src/test/java/com/likelion/news/service/ShellRunnerServiceTest.java
@@ -0,0 +1,27 @@
+package com.likelion.news.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ShellRunnerServiceTest {
+
+    private ShellRunnerService shellRunnerService = new ShellRunnerService();
+
+    @Test
+    @DisplayName("command와 arg를 받아 실제 실행하는 명령어를 만들 수 있다.")
+    void t1() throws Exception {
+        //given
+        String testCommand = "echo";
+        String[] testArgs = new String[]{"hello"};
+        //when
+        String result = (String)(ReflectionTestUtils.invokeMethod(shellRunnerService, "createExecuteCommand", testCommand, testArgs));
+        //then
+        assertThat(result).isEqualTo("echo hello");
+
+    }
+    
+    
+}
