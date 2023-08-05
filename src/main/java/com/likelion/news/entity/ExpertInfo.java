3b06d85e278b19ea888187aefeeea588bcfb5a94
Junad-Park <qkrwnsgk1624@icloud.com>
2023-08-05 20:16:23 +0900

    Feat : Expert 신청을 위한 Entity,  Repository 추가, 수정

--- src/main/java/com/likelion/news/entity/ExpertInfo.java
+++ src/main/java/com/likelion/news/entity/ExpertInfo.java
@@ -1,5 +1,6 @@
 package com.likelion.news.entity;
 
+import com.likelion.news.entity.enums.ExpertState;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,18 @@ public class ExpertInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long expertInfoId;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private String job;
     private String company;
     private String businessType;
     private String education;
+
+    @Enumerated(EnumType.STRING)
+    private ExpertState expertState;
 
     
 }
