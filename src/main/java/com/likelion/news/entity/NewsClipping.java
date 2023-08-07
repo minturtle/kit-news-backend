548dbf00830b4587ef25baf351d04c52318363db
minturtle <minseok0425@kakao.com>
2023-08-07 10:34:27 +0900

    Feat : newsClipping 객체 추가

---
+++ src/main/java/com/likelion/news/entity/NewsClipping.java
@@ -0,0 +1,26 @@
+package com.likelion.news.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class NewsClipping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private RefinedNews clippedNews;
+}
