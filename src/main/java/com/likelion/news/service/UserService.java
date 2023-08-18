74a0266aa2ac1e29d29a5a6f15b9c9c9a4f9982a
Junad-Park <qkrwnsgk1624@icloud.com>
2023-08-18 18:23:32 +0900

    feat : userInfoDto에 expertState 추가

--- src/main/java/com/likelion/news/service/UserService.java
+++ src/main/java/com/likelion/news/service/UserService.java
@@ -1,14 +1,19 @@
 package com.likelion.news.service;
 
 import com.likelion.news.dto.UserInfoDto;
+import com.likelion.news.entity.ExpertInfo;
 import com.likelion.news.entity.User;
 
+import com.likelion.news.entity.enums.ExpertState;
 import com.likelion.news.exception.NotFoundException.NoUserException;
+import com.likelion.news.repository.ExpertInfoRepository;
 import com.likelion.news.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +21,21 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final ExpertInfoRepository expertInfoRepository;
 
     public UserInfoDto getUserInfo(String uid){
         User findUser = userRepository.findUserByUid(uid).orElseThrow(NoUserException::new);
+        Optional<ExpertInfo> isExpert = expertInfoRepository.findByUserUid(uid);
+        ExpertState expertState = ExpertState.NONE;
+        if(isExpert.isPresent()){
+            expertState = isExpert.get().getState();
+        }
 
         return UserInfoDto.builder()
                 .name(findUser.getName())
                 .email(findUser.getEmail())
                 .nickname(findUser.getNickname())
+                .expertState(expertState)
                 .build();
     }
 
